4bd468a8d293f0beca2d663f0c0efd84b97f56b5
Romilson <junio.romilson@gmail.com>
2022-06-18 14:19:01 -0300

    actuator config refresh

--- hr-worker/src/main/java/com/rom/hrhorker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/rom/hrhorker/resources/WorkerResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,17 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rom.hrhorker.entities.Worker;
 import com.rom.hrhorker.repositories.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
+	@Value("${test.config}")
+	private String testeConfig;
+	
 	@Autowired
 	private Environment environment;
 	
 	@Autowired
 	private WorkerRepository workerRepository;
+
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<String> configs(){
+		logger.info("CONFIG = "+ testeConfig);
+		return ResponseEntity.ok(testeConfig);
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> listAll(){
