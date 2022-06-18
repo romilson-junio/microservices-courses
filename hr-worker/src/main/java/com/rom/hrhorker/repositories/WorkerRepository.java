3033e982a8926e9cad4605a781aec68d727d49e5
Romilson <junio.romilson@gmail.com>
2022-06-17 21:53:53 -0300

    implementação hr-worker

---
+++ hr-worker/src/main/java/com/rom/hrhorker/repositories/WorkerRepository.java
@@ -0,0 +1,11 @@
+package com.rom.hrhorker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rom.hrhorker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+	
+	
+}
