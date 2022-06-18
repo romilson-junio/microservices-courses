ac2295a6eb529775ad22fb68864215c85a04ebb1
Romilson <junio.romilson@gmail.com>
2022-06-18 12:20:33 -0300

    Hystrix

--- hr-payroll/src/main/java/com/rom/hrpayroll/entities/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/rom/hrpayroll/entities/resources/PaymentResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.rom.hrpayroll.entities.Payment;
 import com.rom.hrpayroll.entities.services.PaymentService;
 
@@ -17,10 +18,17 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService paymentService;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAltenative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> findPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = paymentService.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
+	
+	public ResponseEntity<Payment> getPaymentAltenative( Long workerId, Integer days){
+		Payment payment = new Payment("Teste", 400.0, days);
+		return ResponseEntity.ok(payment);
+	}
+	
 	
 }
