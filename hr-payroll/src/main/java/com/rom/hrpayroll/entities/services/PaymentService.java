600a155f8693b8103439cf9b4eabf8c6e99fea02
Romilson <junio.romilson@gmail.com>
2022-06-17 23:27:29 -0300

    Implementação balanceamento de carga

--- hr-payroll/src/main/java/com/rom/hrpayroll/entities/services/PaymentService.java
+++ hr-payroll/src/main/java/com/rom/hrpayroll/entities/services/PaymentService.java
@@ -1,30 +1,20 @@
 package com.rom.hrpayroll.entities.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.rom.hrpayroll.entities.Payment;
 import com.rom.hrpayroll.entities.Worker;
+import com.rom.hrpayroll.feignClients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
 	@Autowired
-	private RestTemplate restTemplate;
-	
-	@Value("${hr-worker.host}")
-	private String workerHost;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariable = new HashMap<>();
-		uriVariable.put("id", String.valueOf(workerId));
-		uriVariable.put("days", String.valueOf(days));
-		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariable);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 	
--- hr-worker/src/main/java/com/rom/hrhorker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/rom/hrhorker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.rom.hrhorker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,11 @@ import com.rom.hrhorker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment environment;
+	
 	@Autowired
 	private WorkerRepository workerRepository;
 	
@@ -27,6 +35,7 @@ public class WorkerResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		logger.info("Port = " + environment.getProperty("local.server.port"));
 		Worker worker = workerRepository.findById(id).get();
 		return ResponseEntity.ok(worker);
 	}
