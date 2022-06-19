c8978bd1e1a2bd57717b3c29937f7aaec83dc96a
Romilson <junio.romilson@gmail.com>
2022-06-18 23:18:30 -0300

    Configurações de segurança

--- hr-oauth/src/main/java/com/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.hroauth.entities.User;
 import com.hroauth.feignclients.UserFeigClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -24,6 +27,18 @@ public class UserService {
 			throw new IllegalArgumentException("Email not found");
 		}
 		logger.info("Email encontrado: "+email);
+		return user;
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeigClient.findByEmail(username).getBody();
+		
+		if(user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email encontrado: " + username);
 		return user;
 	}
 	
